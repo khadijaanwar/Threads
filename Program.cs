25eecafcb24040c4f32bf244464c49d196b803b9
khadijaanwar <khadijaanwar1730@gmail.com>
2025-09-23 20:58:39 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your name:");
+        string name = Console.ReadLine();
+        for (int i = 5; i>= 0; i--)
+        {
+            Console.WriteLine(i);
+            if(i == 5)
+            {
+                Thread.Sleep(500);
+            }
+            else
+            {
+                Thread.Sleep(1000);
+            }
+        }
+        Console.WriteLine("Welcome " + name+ "!!!");
+    }
+}
+
